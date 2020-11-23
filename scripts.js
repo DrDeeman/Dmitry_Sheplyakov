b8d544bc37daa6564f02d47b20e5d5ab6bc313f5
My Name <stalkerdrdeeman@gmail.com>
2020-11-23 19:28:13 +0300

    Элементы попапа реализованы в виде простейшего компонента

--- scripts.js
+++ scripts.js
@@ -7,26 +7,71 @@ const min = Math.ceil(0);
 const max = Math.floor(5);
 let masIndex = [];
 //хранилище товаров в корзине
-let mapCount = new Map();
+let componentPopup=new Map();
+
+
+(function (window){
+	function Component(object){  
+	this.object = object;
+	this.string;
+	this.col;
+	this.init();
+		this.re_init=function(ob=document.getElementsByClassName(this.object.name)[0].getElementsByTagName('input')[0]){
+		ob.value =this.col;
+		
+	};
+	
+		this.destroy=function(key){
+		document.getElementsByClassName(object.name)[0].parentElement.parentElement.remove();
+		componentPopup.delete(key);
+	}
+	
+	
+	}
+
+	Component.prototype.init=function(){
+		this.col=1;
+		this.string = `<div class='create_div'>
+	  <img src=${this.object.img} height='100%' width='15%'/>
+	  <div class='cart_product'>
+	  <p><b>Наименование товара</b>: комикс ${this.object.name}</p><p><b>Цена товара</b>: ${this.object.countFrom} $</p><p class=${this.object.name}><b>Количество товара</b>: 
+	  <button onclick='deleteProduct(this);'>-</button>
+	<input type='button' disabled value=${this.col}/>
+	  <button onclick='sumWrite(this.parentElement.className);'>+</button></p>
+	  </div><button onclick='deleteProduct(this);'><img src='image/trash.png'</button>
+	  </div>`;
+		document.getElementsByClassName('popup')[0].innerHTML +=this.string;
+
+	};
+
+	
+	
+	
+	window.Component = Component;
+})(window);
+
+
+
+
+
 
 const deleteProduct = ob => {
 	//document.getElementsByClassName['popup'].getElementsByTageName('div')
 	const [t] = document.getElementsByClassName('popup');
 	for (let i in t.children) {
-		if (ob.parentElement == t.children[i]) {//Здесь получаю массив дивов в попапе для обнаружения, в каком диве была нажата кнопка
+		if (ob.parentElement.parentElement.parentElement == t.children[i]) {//Здесь получаю массив дивов в попапе для обнаружения, в каком диве была нажата кнопка
 			//Если родительский див равен диву в массиве, то
 			let index = 2;                    //индекс равен 2, т.к. не учитываем крестик выхода из попапа и заголовок
-			mapCount.forEach((value, key) => {//Затем циклом пробегаем по хранилищу корзины, порядок в корзине соответствует пордку в мапе
+			componentPopup.forEach((value, key) => {//Затем циклом пробегаем по хранилищу корзины, порядок в корзине соответствует пордку в мапе
 
 				if (index == i) {//Если индекс равен индексу дива
 					if (value.col > 1) {//и количество товаров ондого вида больше 1, то просто уменьшаем количество
 						--value.col;
-
-						document.getElementsByClassName(key)[0].innerHTML = `<b>Количество товара</b>:${value.col}`;
+				
+						value.re_init(ob.nextElementSibling);
 
 					} else {
-						mapCount.delete(key); //Или вырезаем все
-						ob.parentElement.remove();
+						componentPopup.get(key).destroy(key);
 					};
 				};
 				index++;
@@ -40,26 +85,21 @@ const deleteProduct = ob => {
 //Эта функция отвечает за динамическое добавление товара в корзину
 const sumWrite = t => {
 	document.getElementsByClassName("pay-cart__count-pay")[0].innerHTML = ++summar;
-	if (!mapCount.has(t)) {//Если имя рандомной карточки не соответствует ключу в мапе, то
+	if (!componentPopup.has(t)) {//Если имя рандомной карточки не соответствует ключу в мапе, то
 		masCard.forEach((item) => {//Добавляем товар извлекая его из общего набора
 			if (t == item.name) {
-				item.col = 1;//добавляем поле с количеством
-				mapCount.set(t, item);
-				const buttonStroke = `<button onclick='deleteProduct(this);'><img src='image/trash.png'</button>`;//Тут просто шаблонная строка
+				
 
-				document.getElementsByClassName('popup')[0].innerHTML += `<div class='create_div'>
-	  <img src=${item.img} height='100%' width='15%'/>
-	  <div class='cart_product'>
-	  <p><b>Наименование товара</b>: комикс ${t}</p><p><b>Цена товара</b>: ${item.countTo}</p><p class=${item.name}><b>Количество товара</b>: ${item.col}</p>
-	  </div>${buttonStroke}
-	  </div>`;
+	        componentPopup.set(t,new Component(item));
 
 			}
 		});
 
 	} else {
-		mapCount.get(t).col += 1;
-		document.getElementsByClassName(t)[0].innerHTML = "<b>Количество товара</b>:" + mapCount.get(t).col;
+		componentPopup.get(t).col += 1;
+
+		componentPopup.get(t).re_init();
+		//document.getElementsByClassName(t)[0].innerHTML = "<b>Количество товара</b>:" + mapCount.get(t).col;
 	}
 }
 
